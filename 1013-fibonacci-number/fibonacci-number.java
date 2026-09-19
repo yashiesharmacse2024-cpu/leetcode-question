1a388b2b61b6853bd7ba8d714e68628bbe8b81b4
yashiesharmacse2024-cpu <yashie.sharma.cse.2024@miet.ac.in>
2026-09-19 11:59:35 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (70.31%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int prev2 = 0;
+        int prev1 = 1;
+        int current = 0;
+        for (int i = 2; i <= n; i++) {
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return current;
+        
+    }
+}
